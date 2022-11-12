4bfa93f8a7c5a9dea1883897a9a874a05ee7797f
malken21 <Malken_Marumasa@outlook.jp>
2022-11-12 11:10:44 +0900

    Add files via upload

--- main.php
+++ main.php
@@ -1,6 +1,18 @@
 <?php
 
-if ($_SERVER['HTTP_USER_AGENT'] == "") exit;
+# リダイレクトさせないようにする
+$blocklist = ["http"];
+
+#  User-Agent に $blocklist に書いている部分があったら
+# $blockresult に配列を追加する
+$blockresult = array_filter($blocklist, function ($text) {
+    return strpos($_SERVER['HTTP_USER_AGENT'], $text);
+});
+
+# User-Agent に $blocklist に書いている部分があれば 処理を終了する
+if (count($blockresult) != 0) exit;
+
+
 
 # GETパラメーター名
 $Parameter = "?url=";
@@ -15,7 +27,6 @@ if (count($Redirect) == 1) {
     exit;
     # ここから下の処理終了
 }
-
 
 # 一番前の要素を削除 (GETパラメーター以外の部分[URL] を削除)
 unset($Redirect[0]);
